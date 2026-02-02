1f6056889bba6880fa069498f5948eec974d3a37
MiguelMagior <miguel.amagior@gmail.com>
2026-02-02 12:20:18 -0300

    fix: correção no tratamento de exceções

--- teste4/backend/main.py
+++ teste4/backend/main.py
@@ -10,10 +10,12 @@ app = FastAPI(
     version="1.0.0"
 )
 
+
 # Test Route
 @app.get("/")
 def home():
     return {"message": "A API está funcionando"}
+
 
 # Route 1: list all companies (pagination)
 @app.get("/api/operadoras")
@@ -22,34 +24,37 @@ def list_operadoras(
         limit: int = Query(10, ge=1, le=100, description="Itens por página"),
         db: Session = Depends(get_db)
 ):
-    offset = (page - 1) * limit
+    try:
+        offset = (page - 1) * limit
 
-    query = db.query(
-        Operadora.cnpj,
-        Operadora.razao_social
-    )
-    total = query.count()
+        query = db.query(
+            Operadora.cnpj,
+            Operadora.razao_social
+        )
+        total = query.count()
 
-    operadoras = query \
-        .order_by(Operadora.razao_social) \
-        .offset(offset) \
-        .limit(limit) \
-        .all()
+        operadoras = query \
+            .order_by(Operadora.razao_social) \
+            .offset(offset) \
+            .limit(limit) \
+            .all()
 
-    result = [
-        {
-            "razao_social": op.razao_social,
-            "cnpj": op.cnpj,
+        result = [
+            {
+                "razao_social": op.razao_social,
+                "cnpj": op.cnpj,
+            }
+            for op in operadoras
+        ]
+
+        return {
+            "data": result,
+            "total": total,
+            "page": page,
+            "limit": limit
         }
-        for op in operadoras
-    ]
-
-    return {
-        "data": result,
-        "total": total,
-        "page": page,
-        "limit": limit
-    }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 # Route 2: search company by CNPJ
@@ -57,13 +62,16 @@ def list_operadoras(
 def search_operadora(
         cnpj: str,
         db: Session = Depends(get_db)):
-    operadora = (db.query(Operadora)
-                .filter(Operadora.cnpj == cnpj)
-                .first())
-    if not operadora:
-        raise HTTPException(status_code=404, detail="Operadora não encontrada")
+    try:
+        operadora = (db.query(Operadora)
+                     .filter(Operadora.cnpj == cnpj)
+                     .first())
+        if not operadora:
+            raise HTTPException(status_code=404, detail="Operadora não encontrada")
 
-    return operadora
+        return operadora
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 # Route 3: company expenses
@@ -72,21 +80,18 @@ def lits_despesas_operadora(
         cnpj: str,
         db: Session = Depends(get_db)
 ):
-    operadora = db.query(Operadora).filter(Operadora.cnpj == cnpj).first()
-    if not operadora:
-        raise HTTPException(status_code=404, detail="Operadora não encontrada")
+    try:
+        operadora = db.query(Operadora).filter(Operadora.cnpj == cnpj).first()
+        if not operadora:
+            raise HTTPException(status_code=404, detail="Operadora não encontrada")
 
-    despesas = (db.query(DespesaConsolidada)
-                .filter(DespesaConsolidada.cnpj == cnpj)
-                .all())
+        despesas = (db.query(DespesaConsolidada)
+                    .filter(DespesaConsolidada.cnpj == cnpj)
+                    .all())
 
-    total = db.query(DespesaConsolidada).filter(DespesaConsolidada.cnpj == cnpj).count()
-
-    return {
-        "operadora": operadora.razao_social,
-        "despesas": despesas,
-        "total": total,
-    }
+        return despesas
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 # Route 4: statistics
@@ -95,20 +100,20 @@ def list_statistics(db: Session = Depends(get_db)):
     try:
         stats = db.query(
             func.sum(DespesaAgregada.valor).label("soma_total"),
-            func.round(func.avg(DespesaAgregada.valor),2).label("media_geral"),
+            func.round(func.avg(DespesaAgregada.valor), 2).label("media_geral"),
         )
 
         top5 = (db.query(
             DespesaAgregada.cnpj,
             Operadora.razao_social,
             func.sum(DespesaAgregada.valor).label("valor_total"),
-            func.round(func.avg(DespesaAgregada.valor),2).label("media")
+            func.round(func.avg(DespesaAgregada.valor), 2).label("media")
         ).join(
             Operadora, DespesaAgregada.cnpj == Operadora.cnpj
         ).group_by(DespesaAgregada.cnpj)
-        .order_by(desc("valor_total"))
-        .limit(5)
-        .all())
+                .order_by(desc("valor_total"))
+                .limit(5)
+                .all())
 
         soma_total = stats[0].soma_total
         media_geral = stats[0].media_geral
@@ -123,7 +128,7 @@ def list_statistics(db: Session = Depends(get_db)):
                     "total_despesas": item.valor_total,
                     "media_valor_despesa": item.media,
                 }
-                for item in top5 ]
+                for item in top5]
         }
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
